89a0a6c7f6d53f5c04b4753d93fa2ddef4c7452d
Douman <douman@gmx.se>
2021-05-08 16:07:10 +0900

    Check double init

--- tests/sem.rs
+++ tests/sem.rs
@@ -1,6 +1,24 @@
 use semka::Sem;
 
 #[test]
+fn should_fail_init_twice() {
+    let sem = unsafe {
+        Sem::new_uninit()
+    };
+
+    assert!(sem.init(0));
+    assert!(!sem.init(0));
+
+    assert!(!sem.try_wait());
+    sem.signal();
+    sem.signal();
+    assert!(sem.try_wait());
+    assert!(sem.try_wait());
+
+    assert!(!sem.try_wait());
+}
+
+#[test]
 fn should_return_when_signaled_counting() {
     let sem = Sem::new(0).unwrap();
 
